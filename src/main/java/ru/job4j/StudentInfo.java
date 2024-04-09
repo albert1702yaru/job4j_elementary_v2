53b2bc3d22867f327ebb21206dd1fe655da4b5b4
Alber <albert1702@ya.ru>
2024-04-09 07:39:09 +0300

    4.1. Разбор первой программы. [#159033 #420638]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Исманский Альберт");
+        System.out.println("30.05.1985");
+    }
+}
